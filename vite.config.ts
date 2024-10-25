565eae46ca6d77c53a475dcaeeb206c19f937706
zeolsem <zsolim@proton.me>
2024-10-25 23:57:40 +0200

    hotfix for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/rekrutacja-solvro-2024-frontend/',
+  base: 'https://zeolsem.github.io/rekrutacja-solvro-2024-frontend/',
 })
